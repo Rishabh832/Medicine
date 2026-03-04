fd1a243fd2aa7f218f366f4df8d48a646530213a
Rishabh <rkatiyar318@gmail.com>
2026-03-04 18:32:30 +0530

    fix build

--- Frontend/Medicine/vite.config.js
+++ Frontend/Medicine/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/static/frontend/',   
   build: {
     outDir: '../../Backends/Medicine/static/frontend',
     emptyOutDir: true,
