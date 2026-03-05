d977eaa6c343d57ccec56652e67d33c97b6fc000
Rishabh <rkatiyar318@gmail.com>
2026-03-05 23:36:09 +0530

    g

--- Backends/Medicine/Medicine/settings.py
+++ Backends/Medicine/Medicine/settings.py
@@ -157,9 +157,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 import cloudinary
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('dcrispfn7'),
-    'API_KEY': os.environ.get('734723636864287'),
-    'API_SECRET': os.environ.get('IcX2dp867ZBrLKYtoQOseq1zKbE'),
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY':    os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
